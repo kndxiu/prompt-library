966c6ab55e40741306bc9e8b993d25017b158541
kndxiu <95412042+kndxiu@users.noreply.github.com>
2025-12-21 16:27:11 +0100

    Fix version number

--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -1,4 +1,4 @@
 export { ThemeProvider } from "./context";
 export { useTheme } from "./useTheme";
 export { detectHostTheme, watchHostTheme } from "./detector";
-export type { ThemeMode, ThemeContextType, ThemeColors } from "./types";
+export type { ThemeMode, ThemeContextType } from "./types";
